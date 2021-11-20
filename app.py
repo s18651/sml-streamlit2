57daec27a5ac2d81e6907c70ed2b56eed637014c
s18651 <80163181+s18651@users.noreply.github.com>
2021-11-20 16:01:48 +0100

    Update target column value
    Based on data a person with more sickness will not be cured in a week so "Zdrowie" should be "Sickness"

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def main():
 	s_confidence = model.predict_proba(data)
 
 	with prediction:
-		st.header("Czy wyzdrowiejesz w ciągu tygodnia? {0}".format("Tak" if survival[0] == 1 else "Nie"))
+		st.header("Czy wyzdrowiejesz w ciągu tygodnia? {0}".format("Tak" if survival[0] == 0 else "Nie"))
 		st.subheader("Pewność predykcji {0:.2f} %".format(s_confidence[0][survival][0] * 100))
 
 if __name__ == "__main__":
